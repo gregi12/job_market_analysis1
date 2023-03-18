d57027b1b594650df3d42ac1c84fa426df12c1de
gregi12 <114067438+gregi12@users.noreply.github.com>
2023-03-18 18:05:40 +0100

    sada

--- pages/4_Work_from_home.py
+++ pages/4_Work_from_home.py
@@ -3,11 +3,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 import seaborn as sns
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+# Utwórz ścieżkę do pliku csv używając ścieżki bazowej i folderu files
+png_path = os.path.join(BASE_DIR, '..', 'files', 'out.png')
 
 
 tab1, tab2 , tab3= st.tabs(["Quantity", "Percentage", "Table"])
 with tab1:
+    st.write(png_path)
     
     
 
