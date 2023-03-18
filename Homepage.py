a5841626ec19ef7daf7b38b42f66868f02becc88
gregi12 <114067438+gregi12@users.noreply.github.com>
2023-03-18 15:34:45 +0100

    fgf

--- Homepage.py
+++ Homepage.py
@@ -5,7 +5,7 @@ import numpy as np
 import altair as alt
 import matplotlib.pyplot as plt
 st.set_option('deprecation.showPyplotGlobalUse', False)
-mpl.style.use('classic')
+
 st.set_page_config(
   page_title='Job market analsysis',
   page_icon = '📊'
