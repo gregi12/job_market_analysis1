2015250eed22a6988a5a9ef48a20f6c050ed4e6d
gregi12 <114067438+gregi12@users.noreply.github.com>
2023-03-21 15:40:24 +0100

    srsasd

--- pages/2_Salary.py
+++ pages/2_Salary.py
@@ -15,10 +15,10 @@ df = pd.read_csv(dolary_path)
 df1= pd.read_csv(funty_path)
 
 st.write('Choose currency or overall')
-tab1, tab2 , tab3,tab4= st.tabs("Overview","Dollars", "Pounds", "Avearge salary")
+tab1,tab2,tab3,tab4= st.tabs("Overview","Dollars", "Pounds", "Average salary")
 with tab1:
-    st.write("Here is code which took us from something like this: '£35,000 - £55,000','£30K–£41K a year', '45–75 an hour','70K–80K a year', '$75K–$85K a year' to what you see in next tabs. ")
-    st.expander()
+    st.write("Below is link to code which took us from something like this: '£35,000 - £55,000','£30K–£41K a year', '45–75 an hour','70K–80K a year', '$75K–$85K a year' to what you see in next tabs. ")
+    st.expander('Check if u want!')
     
 with tab2:
     st.subheader('Salary ranges in dollars 📊')
