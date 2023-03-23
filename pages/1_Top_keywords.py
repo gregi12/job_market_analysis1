b0817299ab2feb9dd13f19b68a51777a18571f2f
gregi12 <114067438+gregi12@users.noreply.github.com>
2023-03-23 11:32:10 +0100

    dasd

--- pages/1_Top_keywords.py
+++ pages/1_Top_keywords.py
@@ -21,7 +21,7 @@ tab1, tab2 , tab3= st.tabs(["Quantity", "Percentage", "Table"])
 with tab1:
   
   st.subheader('Top 15 keywords by amount 📊')
-  colors = ['#F21D1D', '#F12923' , '#2439E1','#2962F8','#F7653B','#F8CA29','#FDDC48'] 
+  colors = ['#F21D1D', '#F12923' , '#2439E1','#2962F8','#F64F20','#F7653B','#F8CA29','#FDDC48'] 
   fig = plt.figure()
   plt.bar(df['keywords'], df['amount'],color=colors, edgecolor='black')
   plt.xticks(rotation=90)
