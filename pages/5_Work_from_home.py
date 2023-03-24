78de144fb446b8bf17f86625a109c472e74ad522
gregi12 <114067438+gregi12@users.noreply.github.com>
2023-03-24 16:09:48 +0100

    sadsad

--- pages/5_Work_from_home.py
+++ pages/5_Work_from_home.py
@@ -12,13 +12,5 @@ png_path = os.path.join(BASE_DIR, '..', 'files', 'out.png')
 tab1, tab2 = st.tabs(["Quantity", "Percentage"])
 with tab1:
     st.image(png_path)
-with tab2:
-    data = {"work_from_home":[21,79]}
-    df = pd.DataFrame(data)
-    sns.set(style='whitegrid')
-    plt.figure(figsize=(10, 6))  # Adjust the figure size
-    ax = sns.barplot(x='work_from_home', y='percentage', data=df)
-    ax.set(xlabel='work from home', ylabel='percentage', title='work from home')
-    st.pyplot(ax)
 
    
