35e95834000f7d0f5ea3685d6ec91783bdf5cb1a
gregi12 <114067438+gregi12@users.noreply.github.com>
2023-03-24 17:15:28 +0100

    dsa

--- pages/5_Work_from_home.py
+++ pages/5_Work_from_home.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
-import seaborn as sns
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Utwórz ścieżkę do pliku csv używając ścieżki bazowej i folderu files
@@ -13,4 +13,20 @@ tab1, tab2 = st.tabs(["Quantity", "Percentage"])
 with tab1:
     st.image(png_path)
 
-   
+with tab2:
+
+    # Data for the pie chart
+    labels = ['No', 'Yes']
+    sizes = [79, 21]
+    colors = ['#66b3ff', '#ff9999']
+
+    # Create the pie chart
+    fig, ax = plt.subplots()
+    ax.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90)
+
+    # Set the title
+    ax.set_title('Work From Home')
+
+    # Equal aspect ratio ensures that pie is drawn as a circle
+    ax.axis('equal')
+    st.pyplot(ax)
