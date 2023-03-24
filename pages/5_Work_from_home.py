221542820ca7c86152a01c5f8bf3702173be2cfe
gregi12 <114067438+gregi12@users.noreply.github.com>
2023-03-24 16:06:27 +0100

    dsada

--- pages/5_Work_from_home.py
+++ pages/5_Work_from_home.py
@@ -9,7 +9,16 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 png_path = os.path.join(BASE_DIR, '..', 'files', 'out.png')
 
 
-tab1, tab2 , tab3= st.tabs(["Quantity", "Percentage", "Table"])
+tab1, tab2 = st.tabs(["Quantity", "Percentage"])
 with tab1:
     st.image(png_path)
-    
+with tab2:
+    data = {"work from home":[21,79]}
+    df = pd.DataFrame(data)
+    sns.set(style='whitegrid')
+    plt.figure(figsize=(10, 6))  # Adjust the figure size
+    ax = sns.barplot(x='salary', y='amount', data=df)
+    ax.set(xlabel='work from home', ylabel='percentage', title='work from home')
+    st.pyplot(ax)
+
+   
