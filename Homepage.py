1e1b33be53072376c4a2d16553f195190ba8315f
gregi12 <114067438+gregi12@users.noreply.github.com>
2023-07-19 17:59:31 +0200

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -43,27 +43,7 @@ try:
     st.write('If someone is wondering...')
     with st.expander("Here I explain how I obtained data"):
       st.write("https://github.com/gregi12/job_market_analysis1/blob/main/obtaining_data.py")
-      # Tworzenie trzech przycisków w trzech kolumnach
-    col1, col2, col3 = st.beta_columns(3)
-
-    # Dodawanie przycisków do kolumn
-    with col1:
-      button1 = st.button("About analysis")
-      if button1:
-        original_title = '<p style="font-family:Calibri; font-size: 20px; font-weight:600;">Sooo, these results comes from over 1200 job offers. Above is link to github file where I give more detailed explanation about how I obtained the data. I extracted valuable data using regular expressions (regex)</p>'
-        st.markdown(original_title, unsafe_allow_html=True)
-        with st.expander("More info here!"):
-          st.write("https://github.com/gregi12/job_market_analysis1/blob/main/getting_keywords.py")
-    with col2:
-      if st.button("About app"):
-        original_title = '<p style="font-family:Calibri; font-size: 20px; font-weight:600;">In upper left corner you can see navigation links, every one of them is for specific part of visualization . Whenever there is info about percentage , it means percentage of all offers after duplicates were deleted, so eventually on 994 offers. If not, then I give additional info . I would suggest to use light theme , everything is more clear then. Theme can be changed in settings in upper right corner</p>'
-        st.markdown(original_title, unsafe_allow_html=True)
-          
-
-    with col3:
-      if st.button("About me"):
-        original_title = "<p style='font-family:Calibri; font-size: 20px; font-weight:600;'> I'm a young person with a lot of energy and ideas. I am kind of guy who looks for patterns everywhere. Maybe I don't have strong math background, because I haven't graduate any studies but skills are there for sure, in high school I was laureate of district-level math competition (there were only about 80 students in my voivodeship with the same status). For some time I'm catching up with statistics and linear algebra, on my own . In the future I want to work in AI industry  </p>"
-        st.markdown(original_title, unsafe_allow_html=True)
+   
         
 except:
   pass
